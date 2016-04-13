e32aa21ce52053089d66a9de9bf3734f0686bd97
geyinledi <260221580@qq.com>
2016-04-13 22:27:01 +0800

    add Sqlist

---
+++ Sqllist.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int Index(char a[],char b[],int m)
+{
+    int x,y,i,j,k;
+    x=strlen(a);
+    i=0;
+    while(i<x)
+    {
+        k=i;
+        j=0;
+        while(a[k]==b[j]&&k<x&&j<m)
+        {
+            k++;
+            j++;
+        }
+        if(j==m)
+            return i+1;
+        if(k==x)
+            return 0;
+        i++;
+    }
+    return 0;
+}
+int main()
+{
+    char a[50],b[50];
+    int n,m;
+    scanf("%s %s",a,b);
+    m=strlen(b);
+    while(Index(a,b,m))
+    {
+        n=Index(a,b,m);
+        for(int j=n+m-1;j<strlen(a);j++)
+            a[j-m]=a[j];
+        a[strlen(a)-m]='\0';
+    }
+    printf("%s\n",a);
+    return 0;
+}
